53e94271def5c4dbd2be450cc6b5d6af2ddc8c46
lelleking123 <leo.ostlund@skola.taby.se>
2023-05-19 19:39:22 +0200

    nytt

---
+++ JS/Minsida.js
@@ -0,0 +1,48 @@
+let menuToggle = document.querySelector(".menuToggle");
+let navigation = document.querySelector(".navigation");
+menuToggle.onclick = function () {
+  navigation.classList.toggle("active");
+};
+
+function reveal() {
+  var revealsLeft = document.querySelectorAll(".revealLeft");
+  var revealsRight = document.querySelectorAll(".revealRight");
+  var revealsUp = document.querySelectorAll(".revealUp");
+
+  for (var i = 0; i < revealsLeft.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop1 = revealsLeft[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop1 < windowHeight - elementVisible) {
+      revealsLeft[i].classList.add("active");
+    } else {
+      revealsLeft[i].classList.remove("active");
+    }
+  }
+  for (var i = 0; i < revealsRight.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop2 = revealsRight[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop2 < windowHeight - elementVisible) {
+      revealsRight[i].classList.add("active");
+    } else {
+      revealsRight[i].classList.remove("active");
+    }
+  }
+  for (var i = 0; i < revealsUp.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop3 = revealsUp[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop3 < windowHeight - elementVisible) {
+      revealsUp[i].classList.add("active");
+    } else {
+      revealsUp[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
